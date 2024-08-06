99aa9c37ada948a4b5957a49974f2b6b9d72f8f0
AxelSV50 <axelsv50@gmail.com>
2024-08-05 20:14:21 -0600

    Exercise 2.11

--- Part_2/phonebook/src/App.jsx
+++ Part_2/phonebook/src/App.jsx
@@ -1,17 +1,20 @@
+import { useEffect } from 'react'
 import { useState } from 'react'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Armando Perro', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [nameSearch, setNameSearch] = useState('')
 
+  useEffect(() =>{
+    axios
+      .get('http://localhost:3001/persons')
+      .then((response)=>{
+        setPersons(response.data)
+      })
+  }, [])
   const handleAddName = (e) =>{
 
     e.preventDefault()
